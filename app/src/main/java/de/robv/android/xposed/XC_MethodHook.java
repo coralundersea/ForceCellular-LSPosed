e3c44410c1bbcb327785d629de20689250037a8d
coralundersea <mkhjmy@qq.com>
2026-09-18 09:40:04 +0000

    Fix Xposed stubs: add hookAllMethods and thisObject

--- app/src/main/java/de/robv/android/xposed/XC_MethodHook.java
+++ app/src/main/java/de/robv/android/xposed/XC_MethodHook.java
@@ -1,11 +1,15 @@
 package de.robv.android.xposed;
+
 public abstract class XC_MethodHook {
     public static class MethodHookParam {
         public Object[] args;
         public Object result;
+        public Object thisObject;
+
         public void setResult(Object result) { this.result = result; }
         public Object getResult() { return result; }
     }
+
     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {}
     protected void afterHookedMethod(MethodHookParam param) throws Throwable {}
 }
--- app/src/main/java/de/robv/android/xposed/XposedBridge.java
+++ app/src/main/java/de/robv/android/xposed/XposedBridge.java
@@ -1,5 +1,10 @@
 package de.robv.android.xposed;
+
 public class XposedBridge {
     public static void log(String text) {}
     public static void log(Throwable t) {}
+
+    public static void hookAllMethods(Class<?> clazz, String methodName, XC_MethodHook hook) {
+        // stub for compile time
+    }
 }

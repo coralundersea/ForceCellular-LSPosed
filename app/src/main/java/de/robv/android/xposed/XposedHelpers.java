e327e1aa499395cfc226d4de014ab436b872ed9b
coralundersea <mkhjmy@qq.com>
2026-09-18 09:15:14 +0000

    Fix XposedHelpers stub to support callMethod

--- app/src/main/java/de/robv/android/xposed/XposedHelpers.java
+++ app/src/main/java/de/robv/android/xposed/XposedHelpers.java
@@ -1,8 +1,24 @@
 package de.robv.android.xposed;
+
 public class XposedHelpers {
     public static Class<?> findClass(String className, ClassLoader classLoader) {
-        try { return Class.forName(className, false, classLoader); }
-        catch (Throwable t) { return null; }
+        try {
+            return Class.forName(className, false, classLoader);
+        } catch (Throwable t) {
+            return null;
+        }
     }
-    public static void findAndHookMethod(Class<?> clazz, String methodName, Object... args) {}
+
+    public static void findAndHookMethod(Class<?> clazz, String methodName, Object... args) {
+        // stub for compile time
+    }
+
+    public static Object callMethod(Object obj, String methodName, Object... args) {
+        try {
+            java.lang.reflect.Method m = obj.getClass().getMethod(methodName);
+            return m.invoke(obj);
+        } catch (Throwable t) {
+            return null;
+        }
+    }
 }
